8dad298650a8ae8cc827fb0b97a2a1877c5d5a6c
Robert <cj92robert@gmail.com>
2019-10-03 23:44:48 +0200

    heroku fix6

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + '/dist/pracaDomowaTydzien4>'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/pracaDomowaTydzien4/index.html'));
 });
