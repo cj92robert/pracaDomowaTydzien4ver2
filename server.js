3a771cff15a5104f6621c20c77aab6da95b7bfc6
Robert <cj92robert@gmail.com>
2019-10-03 23:05:23 +0200

    heroku fix6

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000, function () {
+  console.log('Node app is working!');
+});
