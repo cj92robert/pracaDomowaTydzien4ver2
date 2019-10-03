499e3f2a49f4d81a9e3642d0bd1d5725455c1f1c
Robert <cj92robert@gmail.com>
2019-10-03 21:59:12 +0200

    heroku fix4

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const express = require('express');
-const app = express();
+"use strict";
+const express = require("express");
+const compression = require("compression");
 
-app.use(express.static(__dirname+ '/dist'));
-app.listen(process.env.PORT || 8080);
+const _port = 4100;
+const _app_folder = 'dist/application';
+
+const app = express();
+app.use(compression());
+
+// ---- SERVE STATIC FILES ---- //
+app.server.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+  res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+  console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
