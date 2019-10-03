f75018e64e1ab266b3ba87be5569fe2d7d1eb8da
Robert <cj92robert@gmail.com>
2019-10-04 01:28:08 +0200

    heroku fix7

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-
-
-
-
+var express = require('express');
+var path = require('path');
 
 var app = express();
-app.use(bodyParser.json());
 
-// Create link to Angular build directory
-var distDir = __dirname +'/dist/pracaDomowaTydzien4ver2';
-app.use(express.static(distDir));
- //
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/pracaDomowaTydzien4'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/pracaDomowaTydzien4/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
